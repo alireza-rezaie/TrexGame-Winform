a908fc7ade0b8ea333f90cf995fbc1614c2c87ef
unknown <alireza3356864@gmail.com>
2022-04-28 14:17:15 +0430

    t-rex

---
+++ daynacor/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace daynacor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.peropertisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backcolorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(992, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "00:00:00";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(480, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(588, 63);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.peropertisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1084, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // peropertisToolStripMenuItem
+            // 
+            this.peropertisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backcolorToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.peropertisToolStripMenuItem.Name = "peropertisToolStripMenuItem";
+            this.peropertisToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
+            this.peropertisToolStripMenuItem.Text = "properties";
+            this.peropertisToolStripMenuItem.Click += new System.EventHandler(this.peropertisToolStripMenuItem_Click);
+            // 
+            // backcolorToolStripMenuItem
+            // 
+            this.backcolorToolStripMenuItem.Name = "backcolorToolStripMenuItem";
+            this.backcolorToolStripMenuItem.Size = new System.Drawing.Size(157, 26);
+            this.backcolorToolStripMenuItem.Text = "backcolor";
+            this.backcolorToolStripMenuItem.Click += new System.EventHandler(this.backcolorToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(157, 26);
+            this.exitToolStripMenuItem1.Text = "exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::daynacor.Properties.Resources.kak;
+            this.pictureBox4.Location = new System.Drawing.Point(996, 382);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 106);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::daynacor.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 359);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::daynacor.Properties.Resources.zamin2;
+            this.pictureBox3.Location = new System.Drawing.Point(1088, 446);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1089, 62);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::daynacor.Properties.Resources.zamin1;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 446);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1088, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 200;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 200;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1084, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem peropertisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backcolorToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ daynacor/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace daynacor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+ 
+            if (pictureBox4.Bottom > pictureBox1.Top && pictureBox4.Top < pictureBox1.Bottom && (pictureBox4.Left < pictureBox1.Right) && (pictureBox4.Right > pictureBox1.Left))
+
+            {
+                timer1.Enabled = false;
+                timer5.Enabled = false;
+                timer4.Enabled = false;
+                MessageBox.Show("boom");
+                Application.Restart();
+
+
+            }
+            if (timer1.Enabled == true&&timer1.Interval!=1)
+            {
+                pictureBox2.Left -= 10;
+                pictureBox3.Left -= 10;
+                pictureBox4.Left -= 10;
+            }else {
+                pictureBox2.Left -= 20;
+                pictureBox3.Left -= 20;
+                pictureBox4.Left -= 20;
+                timer5.Interval = 100;
+                timer4.Interval = 100;
+            }
+
+
+            if(pictureBox2.Right<0)
+            {
+                pictureBox2.Left = (this.Width - 15);
+                pictureBox4.Left= (this.Width);
+
+                if (timer1.Interval >1)
+                    timer1.Interval -= 7;
+      
+
+
+
+
+
+            }
+            if (pictureBox3.Right < 0)
+            {
+                pictureBox3.Left = (this.Width - 15);
+
+            }
+            
+        }
+
+
+
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        byte h, m, s;
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 100;
+            timer3.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+            timer5.Enabled = true;
+
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer5.Enabled = false;
+            timer4.Enabled = false;
+            timer2.Enabled = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void peropertisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backcolorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void exitToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top == 292)
+            {
+                pictureBox1.ImageLocation = "daynasor/1.png";
+                timer4.Enabled = true;
+                timer5.Enabled = false;
+
+            }
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top == 292)
+            {
+                pictureBox1.ImageLocation = "daynasor/2.png";
+                timer5.Enabled = true;
+                timer4.Enabled = false;
+
+            }
+        }
+
+
+        bool b = true;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            s++;
+            if (s == 60)
+            {
+                s = 0;
+                m++;
+            }
+            if (m == 60)
+            {
+                h++;
+                m = 0;
+
+            }
+            label1.Text = string.Format("{0:00}:{1:00}:{2:00}", h, m, s);
+         
+            if (s==30&&b==true)
+            {
+                this.BackColor = Color.Gray;
+                b = false;
+            }
+      
+
+            
+           
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        //292baraye zamani ke dobar dokme zade mishavad balatar naravad
+            if(e.KeyCode==Keys.Space&&pictureBox1.Top==292) {
+                pictureBox1.Top -= 100;
+                timer3.Enabled = true;
+             
+                
+      
+            }
+
+        }
+             private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
